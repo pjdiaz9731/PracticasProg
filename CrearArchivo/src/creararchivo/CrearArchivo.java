0324e6cd6296e4d86f8e52203c7fe89ac17e7a1b
Pablo Díaz <pdiaz9731@gmail.com>
2023-10-27 12:28:15 -0600

    Practica1

---
+++ CrearArchivo/src/creararchivo/CrearArchivo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package creararchivo;
+
+/**
+ *
+ * @author User
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+import java.util.Formatter;
+import java.util.Properties;
+public class CrearArchivo {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Fichero formulario1 = new Fichero();
+		formulario1.setVisible(true);
+		// TODO code application logic here
+	}
+	
+}
